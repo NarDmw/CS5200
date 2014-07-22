0832caf36be5d81bf78b04cf38242b4624604d6b
Nelson Chiu <nardmw@gmail.com>
2014-07-22 19:40:34 -0400

    Removed Locations Skills Model/Controller/Views

--- app/views/locations_skills/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @locations_skill, :id, :location_id, :skill_id, :user_id, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,6 @@ Rails.application.routes.draw do
 
   resources :locations
 
-  resources :locations_skills
-
   resources :messages
 
   resources :postings
