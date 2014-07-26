d70deafb3fc246a165ad603feec7afa1f7fb7dab
Nelson Chiu <nardmw@gmail.com>
2014-07-26 00:56:18 -0400

    Adds a database count to welcome screen

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,6 @@
 class WelcomeController < ApplicationController
+  def index
+    @num_records = Review.count + User.count + Conversation.count + Message.count + Location.count +
+        Posting.count + FeedbackMessage.count + LocationsSkillsUsers.count + Skill.count
+  end
 end
