5a3b65f78f785d1c825cf61e166556b94e709948
Nelson Chiu <nardmw@gmail.com>
2014-08-10 13:43:18 -0400

    Requirements on creating a new Location

--- app/models/location.rb
+++ app/models/location.rb
@@ -4,6 +4,8 @@ class Location < ActiveRecord::Base
   has_many :users, dependent: :destroy
   has_many :postings
 
+  validates :city, :state, presence: true
+
   def to_s
     "#{city}, #{state}"
   end
