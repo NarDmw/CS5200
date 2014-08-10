aac5df3d0aa85cdfee249d4b483551df9fd633af
Nelson Chiu <nardmw@gmail.com>
2014-08-10 19:16:26 -0400

    FeedbackMessage Creation

--- app/controllers/feedback_messages_controller.rb
+++ app/controllers/feedback_messages_controller.rb
@@ -1,6 +1,6 @@
 class FeedbackMessagesController < ApplicationController
   before_action :set_feedback_message, only: [:show, :edit, :update, :destroy]
-  before_action :confirm_logged_in
+  before_action :confirm_logged_in, except: [:new, :create]
 
   # GET /feedback_messages
   # GET /feedback_messages.json
@@ -16,6 +16,7 @@ class FeedbackMessagesController < ApplicationController
   # GET /feedback_messages/new
   def new
     @feedback_message = FeedbackMessage.new
+    @user_email = User.find(session[:user_id]).email if session[:user_id]
   end
 
   # GET /feedback_messages/1/edit
@@ -26,10 +27,11 @@ class FeedbackMessagesController < ApplicationController
   # POST /feedback_messages.json
   def create
     @feedback_message = FeedbackMessage.new(feedback_message_params)
+    @feedback_message.user_id = session[:user_id]
 
     respond_to do |format|
       if @feedback_message.save
-        format.html { redirect_to @feedback_message, notice: 'Feedback message was successfully created.' }
+        format.html { redirect_to root_path, notice: 'Thanks for your feedback!' }
         format.json { render :show, status: :created, location: @feedback_message }
       else
         format.html { render :new }
@@ -70,6 +72,6 @@ class FeedbackMessagesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def feedback_message_params
-      params.require(:feedback_message).permit(:user_id, :posting_id, :email, :body)
+      params.require(:feedback_message).permit(:posting_id, :email, :body)
     end
 end
