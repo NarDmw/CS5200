e0cc3f429f26050b0c9abc3978443b36a576aa49
Nelson Chiu <nardmw@gmail.com>
2014-08-10 22:25:33 -0400

    Locks permissions for feedback_messages to only current user or admins

--- app/controllers/feedback_messages_controller.rb
+++ app/controllers/feedback_messages_controller.rb
@@ -1,6 +1,6 @@
 class FeedbackMessagesController < ApplicationController
   before_action :set_feedback_message, only: [:show, :edit, :update, :destroy]
-  before_action :confirm_logged_in, except: [:new, :create]
+  before_action lambda{ restrict_permissions(nil) }, except: [:new, :create]
 
   # GET /feedback_messages
   # GET /feedback_messages.json
